965020ceff2bd5c404ac38614868e525afd54bcd
bettertext-project <altattic2@gmail.com>
2026-03-28 03:57:25 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 APP_DIR="$HOME/Bettertext"
 REPO_URL="https://github.com/bettertext-project/better-text-web.git"
-DEB_URL="https://raw.githubusercontent.com/bettertext-project/better-text-web/main/bettertext_1.0.0-2_all.deb"
+DEB_URL="https://github.com/bettertext-project/better-text-web/blob/main/bettertext_1.0.0-2_all.deb
 DEB_PATH="$APP_DIR/bettertext_1.0.0-2_all.deb"
 
 # Colors
